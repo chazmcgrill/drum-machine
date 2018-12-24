adbb6a95167b0a245cee9534409480d76a28ed2d
chazmcgrill <charlietaylorcoder@gmail.com>
2018-12-24 08:48:09 +0000

    Adds Switch Component / Power State

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DrumPads from './DrumPads';
+import Switch from './Switch';
 
-export default () => (
-    <div id="app">
-        <DrumPads />
-        <div>
-            <div id="display">display</div>
-            <div className="dials">dials</div>
-            <div className="buttons">buttons</div>
-        </div>
-    </div>
-);
+class App extends Component {
+    state = {
+        powerOn: false,
+    }
+
+    handlePowerClick = () => {
+        const { powerOn } = this.state;
+        this.setState({ powerOn: !powerOn });
+    }
+
+    render() {
+        const { powerOn } = this.state;
+        return (
+            <div id="app">
+                <DrumPads />
+                <div>
+                    <div id="display">display</div>
+                    <Switch handlePowerClick={this.handlePowerClick} powerOn={powerOn} />
+                    <div className="dials">dials</div>
+                    <div className="buttons">buttons</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Switch.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import './Switch.sass';
+
+export default ({ powerOn, handlePowerClick }) => (
+    <Fragment>
+        <p>Power</p>
+        <div className="switch-wrapper">
+            <p className="switch_tag">ON</p>
+            <div className="switch" onClick={handlePowerClick} role="button" tabIndex="0">
+                <div className="switch_inner" style={{ left: powerOn ? 0 : '25px' }} />
+            </div>
+            <p className="switch_tag">OFF</p>
+        </div>
+    </Fragment>
+);
