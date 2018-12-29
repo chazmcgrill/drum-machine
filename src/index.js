a6608efb13ee97139ff195c9bdfdcd36eb376cdb
chazmcgrill <charlietaylorcoder@gmail.com>
2018-12-29 18:54:39 +0000

    Removes Service Worker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.sass';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('drum-machine'));
-
-serviceWorker.unregister();
