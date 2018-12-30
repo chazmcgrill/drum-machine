56c78a8171190363be142d9c05b70632c654acfa
chazmcgrill <charlietaylorcoder@gmail.com>
2018-12-30 20:41:57 +0000

    Adds Pad Class

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import './DrumPad.sass';
 
-const Pad = ({ padData, handlePadClick }) => (
-    <div
-        className="drum-pad"
-        onClick={() => handlePadClick(padData.id, padData.name)}
-        role="button"
-        tabIndex="0"
-        id={padData.name}
-        style={{
-            backgroundColor: padData.active ? '#51EBF5' : '#46C0C8',
-        }}
-    >
-        {/* <audio src={padData.sound} className="clip" id={padData.id} autoPlay={false} /> */}
-        {padData.id}
-    </div>
-);
+class Pad extends React.Component {
+    constructor(props) {
+        super(props);
+        this.padRef = React.createRef;
+    }
+
+    handlePadClick = () => {
+        this.padRef.currentTime = 0;
+        this.padRef.play();
+    }
+
+    render() {
+        const { padData } = this.props;
+        return (
+            <div
+                className="drum-pad"
+                onClick={() => this.handlePadClick()}
+                role="button"
+                tabIndex="0"
+                id={padData.name}
+                style={{
+                    backgroundColor: padData.active ? '#51EBF5' : '#46C0C8',
+                }}
+            >
+                <audio
+                    ref={(input) => { this.padRef = input; }}
+                    src={padData.sound}
+                    className="clip"
+                    id={padData.id}
+                    autoPlay={false}
+                />
+                {padData.id}
+            </div>
+        );
+    }
+}
 
 export default Pad;
